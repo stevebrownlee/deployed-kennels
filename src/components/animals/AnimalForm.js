8b54e29e1a2dce56b1b56fd91e73cca071ed6c99
Steve Brownlee <stevebrownlee@users.noreply.github.com>
2023-10-13 10:06:24 -0500

    Try router again

--- src/components/animals/AnimalForm.js
+++ src/components/animals/AnimalForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react"
-import { useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom";
 import "./AnimalForm.css"
 import AnimalRepository from "../../repositories/AnimalRepository";
 
